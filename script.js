bed144df1cd8e6a6613f82d8cdd1d1404bbf5843
Adrian Gaile Singh <adriangailee@gmail.com>
2023-04-24 19:01:02 +0800

    initial commit

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ let previous_count = 0;
 
 document.getElementById('increment-btn').addEventListener('click', () => {
 count++;
-document.getElementById('counter').innerText = count;
+document.getElementById('counter').textContent = count;
 });
 
 document.getElementById('reset-btn').addEventListener('click', () => {
 count = 0;
-document.getElementById('counter').innerText = count;
+document.getElementById('counter').textContent = count;
 });
 
 document.getElementById('save-btn').addEventListener('click', () => {
     previous_count = count;
-    document.getElementById('prev-count').innerText = previous_count;
+    document.getElementById('prev-count').textContent = previous_count;
 });
 
 document.getElementById('prev-count').addEventListener('click', () => {
     count = previous_count;
-    document.getElementById('counter').innerText = count;
+    document.getElementById('counter').textContent = count;
 });
